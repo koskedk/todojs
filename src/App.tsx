85db19e30b20d663d6034eafa690d9eb6a21ad99
koskedk <koskedk@live.com>
2024-09-12 12:57:18 +0300

    adding tasks components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Tasks from './Tasks';
 
 function App() {
   return (
     <div>
          <h1>Welcome to toDoJs !</h1>
-      </div>
+         <Tasks/>
+    </div>
   );
 }
 
